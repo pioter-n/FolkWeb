f775e946360fdab7d243217034a3b2664d261161
PiotrB <piotr.baczkiewicz@regitech.pl>
2020-02-11 14:46:46 +0100

    try branch

--- Role/Areas/DanceType/Controllers/DanceTypesController.cs
+++ Role/Areas/DanceType/Controllers/DanceTypesController.cs
@@ -17,6 +17,7 @@ namespace Role.Areas.DanceType.Controllers
 
         public DanceTypesController(ApplicationDbContext context)
         {
+            //
             _context = context;
         }
 
